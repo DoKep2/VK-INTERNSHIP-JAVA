7dc5a8866975716fa930c6221173ba54de96ea34
DoKep <sergo.elisbar@yandex.ru>
2024-03-14 00:00:39 +0300

    add migrations

--- src/main/resources/db/migrations/V2__insert_roles.sql
+++ src/main/resources/db/migrations/V2__insert_roles.sql
@@ -1,1 +1,4 @@
 INSERT INTO role (id, name) VALUES (1, 'USER');
+INSERT INTO role (id, name) VALUES (2, 'POST');
+INSERT INTO role (id, name) VALUES (3, 'ALBUM');
+INSERT INTO role (id, name) VALUES (4, 'ADMIN');
